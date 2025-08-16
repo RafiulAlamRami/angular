2d36c24bbc35cb5a0c58a066782e4e660287b678
RafiulAlamRami <rafiul7368@gmail.com>
2025-08-16 18:26:17 +0600

    use ngmodel data binding

--- data-binding-input-output/src/app/app.ts
+++ data-binding-input-output/src/app/app.ts
@@ -1,16 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InputOutputDatBinding } from './input-output-dat-binding/input-output-dat-binding';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,InputOutputDatBinding],
+  imports: [RouterOutlet,InputOutputDatBinding,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('data-binding-input-output');
   welcomeTitle='Welcome to data binding practice code'
+  fromInput!:string
   parentData='Iam from parent'
   childData!:string
   fromChild(data:string){
